b619d219ae9041b151afe4d549987a6d406b3ebf
Detective Squirrel <2137769+DetectiveSquirrel@users.noreply.github.com>
2024-04-26 19:04:51 +1200

    Update WhereIsMyShrine.cs

--- WhereIsMyShrine.cs
+++ WhereIsMyShrine.cs
@@ -1,7 +1,7 @@
 ﻿using ExileCore;
+using ExileCore.PoEMemory;
 using ExileCore.PoEMemory.Components;
 using ExileCore.PoEMemory.MemoryObjects;
-using ImGuiNET;
 using SharpDX;
 using System.Collections.Generic;
 using System.Linq;
@@ -73,8 +73,7 @@ public class WhereIsMyShrine : BaseSettingsPlugin<Settings>
 
         var ingameUi = GameController.Game.IngameState.IngameUi;
 
-        if (Settings.DisableDrawOnLeftOrRightPanelsOpen &&
-            (ingameUi.OpenLeftPanel.IsVisible || ingameUi.OpenRightPanel.IsVisible))
+        if (Settings.DisableDrawOnLeftOrRightPanelsOpen && (ingameUi.OpenLeftPanel.IsVisible || ingameUi.OpenRightPanel.IsVisible))
         {
             return;
         }
@@ -89,15 +88,38 @@ public class WhereIsMyShrine : BaseSettingsPlugin<Settings>
             return;
         }
 
+        var screenSize = new RectangleF
+        {
+            X = 0,
+            Y = 0,
+            Width = GameController.Window.GetWindowRectangleTimeCache.Size.Width,
+            Height = GameController.Window.GetWindowRectangleTimeCache.Size.Height
+        };
+
         foreach (var shrineEntity in shrineList)
         {
             shrineEntity.TryGetComponent<Shrine>(out var shrineComp);
             shrineEntity.TryGetComponent<Render>(out var renderComp);
 
-            if (shrineComp != null && shrineComp.IsAvailable)
+            if (shrineComp is {IsAvailable: true})
             {
-                Graphics.DrawFilledCircleInWorld(shrineEntity.PosNum, Settings.ShrineSize, Settings.ShrineColor, Settings.ShrineSegments);
+                var entityPosScreen = RemoteMemoryObject.pTheGame.IngameState.Camera.WorldToScreen(shrineEntity.PosNum);
+                if (IsEntityWithinScreen(entityPosScreen, screenSize, 50))
+                {
+                    Graphics.DrawFilledCircleInWorld(shrineEntity.PosNum, Settings.ShrineSize, Settings.ShrineColor, Settings.ShrineSegments);
+                }
             }
         }
+    }
+
+    private static bool IsEntityWithinScreen(Vector2N entityPos, RectangleF screensize, float allowancePX)
+    {
+        // Check if the entity position is within the screen bounds with allowance
+        var leftBound = screensize.Left - allowancePX;
+        var rightBound = screensize.Right + allowancePX;
+        var topBound = screensize.Top - allowancePX;
+        var bottomBound = screensize.Bottom + allowancePX;
+
+        return entityPos.X >= leftBound && entityPos.X <= rightBound && entityPos.Y >= topBound && entityPos.Y <= bottomBound;
     }
 }
